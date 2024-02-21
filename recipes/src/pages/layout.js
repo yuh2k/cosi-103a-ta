87e379c9efc3d8d4a1fc6837ee6656acfe533f8c
makenz <mackenzchung@gmail.com>
2024-02-20 21:29:01 -0500

    fixed team page buttons, added basic grocery list (doesn't save list after closing)

--- recipes/src/pages/layout.js
+++ recipes/src/pages/layout.js
@@ -2,26 +2,50 @@ import {Outlet} from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { useState } from 'react';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import AddTask from './grocerylist/addgroceries.js';
+import TaskList from './grocerylist/grocerylist.js';
+import { TasksProvider } from './grocerylist/grocerycontext.js';
 
+
+
+function Layout() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 // Add a navigation bar to the layout
-const Layout = () => {
   return (
-    <>
-      <Navbar bg="light" data-bs-theme="light" sticky="top" >
-        <Container>
-          <Navbar.Brand to="/">😋Tasty Recipes</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
+      <>
+        <Navbar bg="light" data-bs-theme="light" sticky="top" >
+          <Container>
+            <Navbar.Brand to="/">😋Tasty Recipes</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/team">Meet the Team</Nav.Link>
+              <Nav.Link variant="primary" onClick={handleShow}>
+                Grocery List
+              </Nav.Link>
+            <Offcanvas show={show} onHide={handleClose}>
+           
+              <Offcanvas.Header closeButton>
+              <Offcanvas.Title>Grocery List</Offcanvas.Title>
+              </Offcanvas.Header>
+            
+            <Offcanvas.Body>
+                <TasksProvider>
+                <AddTask />
+                <TaskList />
+              </TasksProvider>
+            </Offcanvas.Body>
+            </Offcanvas>
 
-            <Nav.Link href="/team">Meet the Team</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+            </Nav>
+          </Container>
+        </Navbar>
+        <Outlet />
+      </>
       
-
-      <Outlet />
-    </>
-  )
-};
-
+    )
+  };
 export default Layout;
