446ab09c4c9b7726d89ccb4fe021e2d158e9a0d0
makenz <mackenzchung@gmail.com>
2024-03-12 07:51:37 -0400

    added img for kungpao

--- recipes/src/pages/cookingModepages/kungpaoCooking.js
+++ recipes/src/pages/cookingModepages/kungpaoCooking.js
@@ -7,123 +7,57 @@ function KungpaoCooking() {
       <Carousel.Item>
       <img
           className="d-block w-100"
-          src= "oven.jpeg"
+          src= "https://recipes.net/wp-content/uploads/2024/02/what-is-chicken-marinade-1707280127.jpg"
           alt="First Slide"
-          style={{ width: '800px', height: '400px' }}
+          style={{ width: 'auto', height: '800px' }}
         />
         <Carousel.Caption>
-          <h3>Preheat</h3>
-          <p>Preheat your oven to 325°F (163°C)</p>
+          <h3>Ingredients/Mix</h3>
+          <p>In a bowl, combine soy sauce, rice vinegar, hoisin sauce, and cornstarch.
+             Add the chicken pieces and marinate for about 15-20 minutes.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
       <img
           className="d-block w-100"
-          src= "oven.jpeg"
-          alt="First Slide"
-          style={{ width: '800px', height: '400px' }}
+          src= "https://www.seriouseats.com/thmb/V6jpNqeVGW39ZoTTUhauUPXSbO8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__images__2015__04__20150413-cashew-chicken-ding-shao-zhong-6-189cd45d88cb484988a753b5dbc37a28.jpg"
+          alt="Second Slide"
+          style={{ width: 'auto', height: '800px' }}
         />
         <Carousel.Caption>
-          <h3>Mix Together</h3>
-          <p>Mix graham cracker crumbs and melted butter. Press the mixture into the bottom of a 9-inch springform pan to create the crust</p>
+          <h3>Cook</h3>
+        <p>Heat vegetable oil in a wok or large skillet over medium-high heat. 
+          Add minced garlic and ginger, sauté for about 30 seconds until fragrant.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
       <img
           className="d-block w-100"
-          src= "oven.jpeg"
-          alt="First Slide"
-          style={{ width: '800px', height: '400px' }}
+          src= "https://4.bp.blogspot.com/---y9A1imNJ0/VOYViE_pUJI/AAAAAAAASYs/ZG-GVrbm0mQ/s1600/IMG_9713.JPG"
+          alt="Third Slide"
+          style={{ width: 'auto', height: '800px' }}
         />
         <Carousel.Caption>
-          <h3>Bake</h3>
-          <p>Bake the crust in the preheated oven for 10 minutes. Remove and let it cool </p>
+          <h3>While Cooking</h3>
+          <p>Add marinated chicken to the wok and stir-fry until browned and cooked through.
+          Add diced red and green bell peppers, peanuts, and sliced green onions. </p>
+          <p>Pour in water and chili garlic sauce. Stir-fry for another 1-2 minutes until the sauce thickens.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
       <img
           className="d-block w-100"
-          src= "oven.jpeg"
-          alt="First Slide"
-          style={{ width: '800px', height: '400px' }}
+          src= "https://assets.epicurious.com/photos/62d550372ce044d81614427d/16:9/w_5482,h_3083,c_limit/KungPaoChicken_RECIPE_071422_37114.jpg"
+          alt="Last Slide"
+          style={{ width: 'auto', height: '800px' }}
         />
         <Carousel.Caption>
-          <h3>Mix Some More</h3>
-          <p>In a large mixing bowl, beat the softened cream cheese until smooth. Add sugar and vanilla extract, continue beating until well combined</p>
+          <h3>Serve and Enjoy</h3>
+          <p>Serve the Kung Pao chicken with cooked rice.</p>
+          <p>Dig in to your home cooked Sichuan Kung Pao Chicken!</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-      <img
-          className="d-block w-100"
-          src= "oven.jpeg"
-          alt="First Slide"
-          style={{ width: '800px', height: '400px' }}
-        />
-        <Carousel.Caption>
-          <h3>Eggs!</h3>
-          <p>Add eggs one at a time, beating well after each addition. Mix in the sour cream until the batter is smooth</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img
-          className="d-block w-100"
-          src= "oven.jpeg"
-          alt="First Slide"
-          style={{ width: '800px', height: '400px' }}
-        />
-        <Carousel.Caption>
-          <h3>Add Cream</h3>
-          <p>Pour the cream cheese mixture over the cooled crust in the springform pan</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img
-          className="d-block w-100"
-          src= "oven.jpeg"
-          alt="First Slide"
-          style={{ width: '800px', height: '400px' }}
-        />
-        <Carousel.Caption>
-          <h3>Bake Some More</h3>
-          <p> Bake in the preheated oven for 50-60 minutes or until the center is set. The edges should be slightly golden, and the center should have a slight jiggle </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img
-          className="d-block w-100"
-          src= "oven.jpeg"
-          alt="First Slide"
-          style={{ width: '800px', height: '400px' }}
-        />
-        <Carousel.Caption>
-          <h3>Let Them Cool</h3>
-          <p> Allow the cheesecake to cool in the oven with the door ajar for about an hour. Refrigerate for at least 4 hours or overnight before serving </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img
-          className="d-block w-100"
-          src= "oven.jpeg"
-          alt="First Slide"
-          style={{ width: '800px', height: '400px' }}
-        />
-        <Carousel.Caption>
-          <h3>Remove from Pan</h3>
-          <p> Once chilled, run a knife around the edge of the pan before releasing the springform sides </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img
-          className="d-block w-100"
-          src= "oven.jpeg"
-          alt="First Slide"
-          style={{ width: '800px', height: '400px' }}
-        />
-        <Carousel.Caption>
-          <h3>Enjoy!</h3>
-          <p> Slice and serve your delicious homemade cheesecake! </p>
-        </Carousel.Caption>
-      </Carousel.Item>
+     
     </Carousel>
   );
 }
